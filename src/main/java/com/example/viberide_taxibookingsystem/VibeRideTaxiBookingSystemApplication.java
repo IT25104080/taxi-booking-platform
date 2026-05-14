16cf13fc707ffac2321bfcf585a3432b74501263
IT25104080 <it25104080@my.sliit.lk>
2026-05-14 09:00:00 +0530

    setup spring boot project and maven configuration

---
+++ src/main/java/com/example/viberide_taxibookingsystem/VibeRideTaxiBookingSystemApplication.java
@@ -0,0 +1,11 @@
+package com.example.viberide_taxibookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeRideTaxiBookingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VibeRideTaxiBookingSystemApplication.class, args);
+    }
+}
